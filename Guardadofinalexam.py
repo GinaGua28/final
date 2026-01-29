03b10e0032c36fb96dc631c8e019ecd73013ba3b
GinaGua28 <geg5291@lausd.net>
2026-01-28 18:04:06 -0800

    Add files via upload

---
+++ Guardadofinalexam.py
@@ -0,0 +1,15 @@
+#Guardadofinalexam
+x = int(input("Choose an option 1,2, or 3: "))
+
+if x== 1:
+    name = input("Type in your name: ")
+    print("What do computers snack on? Microchips!")
+
+elif x ==2:
+    for i in range(20):
+        print("BANANA")
+
+elif x ==3:
+    num = int(input("Enter a number: "))
+    while num != 0:
+        num = int(input("Warning! Enter a number: "))
